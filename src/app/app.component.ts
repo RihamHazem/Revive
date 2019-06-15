ce4905877bb4e557ee21ea518270cdf637a56e42
RihamHazem <rihamhazem56@gmail.com>
2019-06-15 19:03:05 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {ColorEvent} from 'ngx-color';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,7 @@ import {ColorEvent} from 'ngx-color';
 })
 export class AppComponent implements OnInit {
   title = 'Revive';
+  divWidth = 500;
   mode = 'upload'; // whether resolution or interactive or colorization
   imageUrl: string|ArrayBuffer;
   imageOffset = {val: -1, dir: false, oldWidth: 0, oldHeight: 0, newWidth: 0, newHeight: 0}; // false: width, true: height
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
   interColorizationMode(imgFile: File) {
     const reader = new FileReader();
     reader.readAsDataURL(imgFile);
-    reader.onload = (event) => {
+    reader.onload = () => {
       this.mode = 'interactive';
       this.imageUrl = reader.result;
       const image = new Image();
@@ -30,17 +30,17 @@ export class AppComponent implements OnInit {
         this.imageOffset.oldHeight = height;
         let offset = -1;
         if (width > height) {
-          offset = (700 / width) * height;
+          offset = (this.divWidth / width) * height;
           this.imageOffset.dir = false;
           this.imageOffset.newWidth = width;
           this.imageOffset.newHeight = offset;
         } else {
-          offset = (700 / height) * width;
+          offset = (this.divWidth / height) * width;
           this.imageOffset.dir = true;
           this.imageOffset.newWidth = offset;
           this.imageOffset.newHeight = height;
         }
-        offset = ((700 - offset) / 2);
+        offset = ((this.divWidth - offset) / 2);
         this.imageOffset.val = offset;
         console.log(this.imageOffset);
       };
